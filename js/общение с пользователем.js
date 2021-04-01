363543a431ddc686d68ba8eae40e8e2f5a99406c
Alexandra <av.ruzhinskaya@gmail.com>
2021-04-01 21:20:07 +0300

    first commit

---
+++ js/общение с пользователем.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/* предупреждение пользователя */
+// alert('Hello');
+
+/* вопрос с вариантами ответа: ок или отмена */
+// const result = confirm('Are you here?');
+// console.log(result);
+
+/* вопрос с возможностью ввода ответа */
+// const answer = +prompt('Вам есть 18?', '18');
+// console.log(answer + 5);
+
+/* заполнение массива ответами пользователя */
+// const answers = [];
+
+ // answers[0] = prompt('Как вас зовут?', '');
+ // answers[1] = prompt('Какая у вас фамилия?', '');
+ // answers[2] = prompt('Сколько вам лет?', '');
+/* проверка типа данных */
+// console.log(typeof(answers));
